09dab25bd3a59daa86a814c8bd20aef2b2055ab8
Adam Burdett <burdettadam@gmail.com>
2026-08-27 09:34:31 -0600

    fix(ui): apply Emotion styles under Tauri CSP

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import App from './App';
+import { appEmotionCache } from './emotionCache';
 
 const theme = createTheme({
   palette: {
@@ -46,9 +48,11 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <CacheProvider value={appEmotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
   </React.StrictMode>
 );
